bb94a7110af5287834701234b107366f6f0e8aa3
Valera Kalinichenko <studiya3@gmail.com>
2023-11-14 09:40:42 +0200

    adaptive

--- js/index.js
+++ js/index.js
@@ -4,7 +4,16 @@ const modal = document.querySelector('.modal');
 const modalBntOpen = document.querySelector('.modal-open-btn');
 const modalBtnClose = document.querySelector('.modal__close-btn');
 
-const ToogleBtn = () => modal.classList.toggle('is-hidden');
+const toogleBtn = () => modal.classList.toggle('is-hidden');
 
-modalBntOpen.addEventListener('click', ToogleBtn);
-modalBtnClose.addEventListener('click', ToogleBtn);
+modalBntOpen.addEventListener('click', toogleBtn);
+modalBtnClose.addEventListener('click', toogleBtn);
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtnOpen = document.querySelector('.menu-btn-open');
+const menyBtnClose = document.querySelector('.menu-btn-close');
+
+const toogleMenu = () => mobileMenu.classList.toggle('is-open');
+
+menuBtnOpen.addEventListener('click', toogleMenu);
+menyBtnClose.addEventListener('click', toogleMenu);
